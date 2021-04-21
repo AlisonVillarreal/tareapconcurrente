79769399ff28c1bd728cb271b7c55ff942623490
alisonvillarreal <alisonvillarreal@upeu.edu.pe>
2021-04-21 18:04:21 -0500

    mandarina

---
+++ src/ejercicio1/Hilo1.java
@@ -0,0 +1,9 @@
+package ejercicio1;
+
+public class Hilo1 extends Thread{
+	public void run() {
+		for (int x = 1; x<=10; x++) {
+			System.out.println(x);
+		}
+	}
+}
---
+++ src/ejercicio1/Hilo2.java
@@ -0,0 +1,10 @@
+package ejercicio1;
+
+public class Hilo2 extends Thread {
+	public void run() {
+		for (int x = 10; x >=1; x--) {
+			System.out.println(x);
+		}
+	}
+
+}
